00346aaf614b1d0fe8aa121766470b6a8136ed9b
aseo <aseo@yahoo-corp.jp>
2021-03-19 16:19:27 +0900

    Initial Commit

---
+++ PekoriObento/PekoriObentoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PekoriObentoApp.swift
+//  PekoriObento
+//
+//  Created by aseo on 2021/03/19.
+//
+
+import SwiftUI
+
+@main
+struct PekoriObentoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
