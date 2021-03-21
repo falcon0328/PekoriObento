2f0347290a7ac034d25204120eb2c82a57286321
aseo <aseo@yahoo-corp.jp>
2021-03-21 12:01:25 +0900

    レシピ表示画面の用意だけ行った

---
+++ PekoriObento/ObentoBakoRecipeListView.swift
@@ -0,0 +1,20 @@
+//
+//  ObentoBakoRecipeListView.swift
+//  PekoriObento
+//
+//  Created by aseo on 2021/03/21.
+//
+
+import SwiftUI
+
+struct ObentoBakoRecipeListView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ObentoBakoRecipeListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ObentoBakoRecipeListView()
+    }
+}
